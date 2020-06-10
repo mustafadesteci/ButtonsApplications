1b1e8a0f6fb7cf56ae6c1c80ee6a065e9771c82c
Mustafa Desteci <45450972+mustafadesteci@users.noreply.github.com>
2020-06-11 00:55:08 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            //button
+            //
+            this.button.Location = new System.Drawing.Point(200, 200);
+            this.button.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(100,100);
+            this.button.TabIndex = 0;
+            this.button.MouseHover += new System.EventHandler(this.button_MouseHover);
+            this.button.Text = "BUTTON";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.Location = new System.Drawing.Point(600, 600);   
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.button);
+            this.Margin = new System.Windows.Forms.Padding(50, 50, 50, 50);
+            this.Name = "Form1";
+            this.Text = "Mustafa DESTECI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You were click");
+            this.Close();
+        }
+
+        Random random = new Random();
+        int step=0;
+        private void button_MouseHover(object sender,EventArgs e)
+        {
+            step += 60;         
+            Button btn = (sender as Button);
+            btn.BackColor = Color.Green;
+            btn.Left = random.Next(0,Width-10);
+            btn.Left = step;  
+            Thread.Sleep(70);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
